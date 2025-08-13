f30ab19f095a0007468b9cbe6260cacab1abbb86
Umatriz <artsharyi@gmail.com>
2025-08-13 08:31:27 +0300

    feat: silplified the CLI creation

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,43 @@
 use std::path::PathBuf;
 
-use clap::{Command, arg, value_parser};
+use clap::{ArgGroup, ArgMatches, Command, arg, value_parser};
 use lilium::{
-    ast::{Expr, expr},
+    ast::{Expr, Parse, Stmt, expr},
     lexer::Lexer,
 };
 
+fn create_command_sources(name: &'static str) -> Command {
+    Command::new(name)
+        .arg(arg!(code: -C --code <CODE>).value_parser(value_parser!(String)))
+        .arg(arg!(file: -F --file <FILE>).value_parser(value_parser!(PathBuf)))
+        .group(
+            ArgGroup::new("sources")
+                .args(["code", "file"])
+                .required(true),
+        )
+}
+
+fn get_sources(matches: &ArgMatches) -> String {
+    if let Some(path) = matches.get_one::<PathBuf>("file") {
+        let content = std::fs::read_to_string(path).unwrap();
+        return content;
+    }
+
+    if let Some(code) = matches.get_one::<String>("code") {
+        return code.clone();
+    }
+
+    unreachable!("No source argument was provided!");
+}
+
 fn main() {
     let matches = Command::new("lilium")
-        .subcommand(
-            Command::new("lex").arg(arg!(file: <FILE>).value_parser(value_parser!(PathBuf))),
-        )
-        .subcommand(
-            Command::new("tree").arg(arg!(code: <CODE>).value_parser(value_parser!(String))),
-        )
+        .subcommand(create_command_sources("lex"))
+        .subcommand(create_command_sources("tree"))
         .get_matches();
 
     if let Some(sub) = matches.subcommand_matches("lex") {
-        let file_path = sub.get_one::<PathBuf>("file").unwrap();
-        let content = std::fs::read_to_string(file_path).unwrap();
+        let content = get_sources(sub);
         println!("--- BEGIN FILE ---\n{content}\n--- END FILE ---");
         let lexer = Lexer::new(&content).unwrap();
         let tokens = lexer.tokens();
@@ -26,18 +45,29 @@ fn main() {
     }
 
     if let Some(sub) = matches.subcommand_matches("tree") {
-        let code = sub.get_one::<String>("code").unwrap();
-        println!("--- BEGIN CODE ---\n{code}\n--- END CODE ---");
+        let content = get_sources(sub);
+        println!("--- BEGIN CODE ---\n{content}\n--- END CODE ---");
 
-        let lexer = Lexer::new(code).unwrap();
-        let tokens = lexer.tokens();
+        let lexer = Lexer::new(&content).unwrap();
+        let mut tokens = lexer.tokens();
         println!("--- BEGIN LEXING RESULTS ---\n{tokens}\n--- END LEXING RESULTS ---");
 
-        let mut expr = expr(&mut lexer.tokens(), 0).unwrap();
+        let mut expr = match Stmt::parse(&mut tokens) {
+            Ok(e) => e,
+            Err(lilium::ast::Error::UnexpectedToken { found, expected }) => {
+                let span = found.span;
+                println!("UNEXPECTED TOKEN: {}", &content[span.start..span.end]);
+                panic!(
+                    "ERROR: {}",
+                    lilium::ast::Error::UnexpectedToken { found, expected }
+                )
+            }
+            Err(err) => panic!("ERROR: {err}"),
+        };
         println!("Tree:");
-        if matches!(expr, Expr::Sequence(_)) {
-            expr = expr.flatten_sequence();
-        }
+        // if matches!(expr, Expr::Sequence(_)) {
+        //     expr = expr.flatten_sequence();
+        // }
         println!("{expr:#?}");
     }
 
